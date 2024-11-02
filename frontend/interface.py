1890cce53badde88c22913322ff974d65147834a
qivaijar <qivaijar@gmail.com>
2024-11-02 16:32:40 +0000

    update copy

--- frontend/interface.py
+++ frontend/interface.py
@@ -1,21 +1,22 @@
 from gradio import Blocks, Markdown, Textbox
 
+
 def refine_word(input_text: str) -> str:
     result = input_text
     return result
 
+
 with Blocks() as i_face:
-        Markdown(
+    Markdown(
         """
         # RefineMyWords
         Start typing sentences you'd like to refine.
         """)
 
-        inp = Textbox(label="Original Text")
-        out = Textbox(label="Refined Text")
+    inp = Textbox(label="Input your original text:")
+    out = Textbox(label="Refined version:")
 
-        inp.change(refine_word, inp, out)
+    inp.change(refine_word, inp, out)
 
 if __name__ == "__main__":
     i_face.launch(share=True)
-    

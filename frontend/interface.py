3e8fea82becfd7795ca2405faec5036b278205d4
qivaijar <qivaijar@gmail.com>
2024-11-05 15:39:43 +0000

    integrate gradio frontend with fastapi backend

--- frontend/interface.py
+++ frontend/interface.py
@@ -1,8 +1,26 @@
-from gradio import Blocks, Markdown, Textbox
+from gradio import Blocks, Markdown, Textbox, Button
+import requests
+from dotenv import load_dotenv
+import os
+
+# Loading environment parameters
+load_dotenv()
+api_url = os.getenv("API_URL")
 
 
 def refine_word(input_text: str) -> str:
-    result = input_text
+    try:
+        refine_url = api_url + '/refine'
+        response = requests.post(
+            refine_url,  # Replace with your FastAPI endpoint
+            json={"sentence": input_text}
+        )
+        # Check if the request was successful
+        response.raise_for_status()
+        result = response.json().get("refined_sentence", "Error: No refined text returned.")
+    except requests.exceptions.RequestException as e:
+        result = f"Error: {e}"
+
     return result
 
 
@@ -14,9 +32,11 @@ with Blocks() as i_face:
         """)
 
     inp = Textbox(label="Input your original text:")
+    submit_btn = Button("Refine My Words!")
     out = Textbox(label="Refined version:")
 
-    inp.change(refine_word, inp, out)
+    # Link the button to the function
+    submit_btn.click(refine_word, inputs=inp, outputs=out)
 
 if __name__ == "__main__":
     i_face.launch(share=True)
